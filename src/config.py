eb5c4912409ff6272ee6b4b90c76f6e459d4fc7b
Gleb_Semenov <gleb_semenov@noreply.gitverse.ru>
2025-06-04 12:32:53 +0000

    update src/config.py

--- src/config.py
+++ src/config.py
@@ -3,12 +3,12 @@ from typing import List, Optional, Set
 
 from dotenv import dotenv_values, load_dotenv
 
+load_dotenv('_env')
+
 config = {
-    **dotenv_values('.env'),
+    **dotenv_values('_env'),
     **os.environ,
 }
-
-load_dotenv()
 
 
 class ConfigError(Exception): ...
